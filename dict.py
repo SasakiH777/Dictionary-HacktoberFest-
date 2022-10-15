91eb58b4dc3bc71aaa8397d2d285564160513ea1
SasakiH777 <115885520+SasakiH777@users.noreply.github.com>
2022-10-15 22:39:50 +0530

    Update dict.py

--- dict.py
+++ dict.py
@@ -7,7 +7,11 @@ dict = {"apple":"A fruit",
        "babbled":"past simple and past participle of babble",
        "babe":"a small baby",
        "babel":"the confusing sound of many people talking at the same time or using different languages",
-       "baboon":"a type of large monkey, found in Africa and Asia, with a long, pointed face like a dog and large teeth"}
+       "baboon":"a type of large monkey, found in Africa and Asia, with a long, pointed face like a dog and large teeth",
+       "cakeage":"a charge that you pay in some restaurants for being allowed to eat a cake or cakes there that you have brought yourself",
+       "cakehole":"a person's mouth",
+       "cakewalk":"something that is very easy to achieve",
+       "cakey":"similar to a cake in how it feels, looks, or tastes"}
 
 
 def meaning(word):
